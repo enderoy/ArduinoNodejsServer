b5d0626e992fcf4739a763a83550369e0b266ade
ender oy <enderoy@gmail.com>
2020-07-27 20:31:47 +0300

    socket.io ile sunucu ve istemci arasında konuşma sağlandı.

--- server4.js
+++ server4.js
@@ -1,11 +1,19 @@
+
+
 const express =require('express');
 const app=express();
 const server=require('http').createServer(app);
+const io=require('socket.io')(server);
+const path=require('path');
 var led;
+var kullanicilar=0;
+
+//Server dan public klasörüne erişim için.
+app.use('/public',express.static(path.join(__dirname,'public')));
+app.use('/node_modules',express.static(path.join(__dirname,'node_modules')));
 //App nesnesi istemcilerden gelen isteği dinlemeye başladı.
 app.get('/',(req,res)=>{
-    res.write("Merhabe ben yeni express sunucun.");
-    res.end();
+    res.sendFile(__dirname+"/index2.html");
 });
 
 app.get('/lediYak',(req,res)=>{
@@ -22,6 +30,7 @@ console.log('3000 nolu port dinlenmeye başladı.');
 
 //SANAL ELEKTRONİK BOARD OLUŞTURDUK.
 const { Board, Led } = require("johnny-five");
+const e = require('express');
 const board = new Board();
 
 board.on("ready", () => {
@@ -30,3 +39,17 @@ board.on("ready", () => {
   led.off();
 });
 //SANAL ELEKTRONİK BOARD OLUŞTURDUK.
+
+io.on('connection',(socket)=>{
+    
+    console.log("Bir kullanıcı bağlandı.");
+    kullanicilar++;
+    console.log(kullanicilar);
+
+    socket.on('disconnect',()=>{
+        kullanicilar--;
+        console.log(kullanicilar);
+    })
+
+})
+
